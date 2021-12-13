0275b6b192fc5f607f7fed5ffffeaca77bf42e01
Neeranuch Jitkhajornwanich <83394452+kpmaiyakarbnam@users.noreply.github.com>
2021-12-13 18:04:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import cv2
 import time
 import datetime
+import os
+import smtplib
+from email.message import EmailMessage
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) 
 
 face_cascade = cv2.CascadeClassifier(
-    cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml") 
 body_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_fullbody.xml")
 
@@ -14,26 +17,64 @@ detection_stopped_time = None
 timer_started = False
 SECONDS_TO_RECORD_AFTER_DETECTION = 5
 
-frame_size = (int(cap.get(3)), int(cap.get(4)))
-fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+frame_size = (int(cap.get(3)), int(cap.get(4))) 
+fourcc = cv2.VideoWriter_fourcc(*"mp4v") 
+current_time = datetime.datetime.now().strftime("%d-%m-%Y-%H-%M")
+out = cv2.VideoWriter(f"{current_time}.mp4", fourcc, 20, frame_size)
 
+def sendvideomail():
+    EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+    EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+    msg = EmailMessage()
+    msg['Subject'] = 'Video capture'
+    msg['From'] = EMAIL_ADDRESS
+    msg['To'] = EMAIL_ADDRESS
+    msg.set_content('Check out new video recording')
+    
+    with open(f"{current_time}.mp4", 'rb') as f:
+        file_data = f.read() 
+        file_name = f.name
+
+    msg.add_attachment(file_data, maintype='video', subtype='mp4', filename=file_name)
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+        smtp.send_message(msg)
+
+def sendmail():
+    EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+    EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+    msg = EmailMessage()
+    msg['Subject'] = 'Video capture'
+    msg['From'] = EMAIL_ADDRESS
+    msg['To'] = EMAIL_ADDRESS
+    msg.set_content('Check out new video recording')
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+        smtp.send_message(msg)
+
+
+#Main loop
 while True:
-    _, frame = cap.read()
+    _, frame = cap.read() 
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5) 
     bodies = body_cascade.detectMultiScale(gray, 1.3, 5)
 
-    if len(faces) + len(bodies) > 0: 
+    if len(faces) + len(bodies) > 0:  
         if detection:
-            timer_started = False
+            timer_started = False 
         else:
             detection = True
-            current_time = datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
-            out = cv2.VideoWriter(f"{current_time}.mp4", fourcc, 20, frame_size)
             print("Started Recording")
     elif detection:
-        if timer_started:
+        if timer_started: 
             if time.time() - detection_stopped_time >= SECONDS_TO_RECORD_AFTER_DETECTION:
                 detection = False
                 timer_started = False
@@ -47,13 +88,15 @@ while True:
         out.write(frame)
 
     for (x, y, width, height) in faces:
-        cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 255, 0), 3) 
+        cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 255, 0), 3)
 
     cv2.imshow("Camera", frame)
 
     if cv2.waitKey(1) == ord('q'):
+        sendmail()
         break
 
+    
 out.release()
 cap.release()
 cv2.destroyAllWindows()
